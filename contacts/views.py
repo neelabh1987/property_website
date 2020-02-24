78d7a0b57f7db13afbb3eeb41938c92392e7c35e
neelabh1987 <krishna.neelabh4@gmail.com>
2020-02-24 14:18:43 +0530

    Disable email

--- contacts/views.py
+++ contacts/views.py
@@ -29,13 +29,13 @@ def contact(request):
         contact.save()
 
         # Send email
-        send_mail(
-            'Property Listing Inquiry',
-            'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
-            'neelabh04071987@gmail.com',
-            [realtor_email, 'kneelabh87@gmail.com'],
-            fail_silently=False
-        )
+        #send_mail(
+           # 'Property Listing Inquiry',
+            #'There has been an inquiry for ' + listing + '. Sign into the admin panel for more info',
+            #'neelabh04071987@gmail.com',
+            #[realtor_email, 'kneelabh87@gmail.com'],
+            #fail_silently=False
+       # )
 
         messages.success(request, 'You request has been submitted, a realtor will get back to you soon')
 
